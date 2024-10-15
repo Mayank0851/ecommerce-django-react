dd553e0831a85f4b0cec4e9670c36ffaac84ff5a
Mayank0851 <vishwakarmamayank451@gmail.com>
2024-10-15 12:33:49 +0530

    update app

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,7 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Redis server URL
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
